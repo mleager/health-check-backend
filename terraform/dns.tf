93f2ab1d9af3d0d5e6b2f0f5be9bf8e74db63931
mark <markleager92@gmail.com>
2025-06-24 23:32:12 -0400

    add dns domain for ALB

---
+++ terraform/dns.tf
@@ -0,0 +1,21 @@
+data "aws_route53_zone" "primary" {
+  name = var.route53_zone_name
+}
+
+resource "aws_route53_recourd" "subdomain_record" {
+  zone_id = data.aws_route53_zone.primary.id
+  name    = "api.${var.route53_zone}"
+  type    = "A"
+
+  alias {
+    name    = aws_lb.alb.dns_name
+    zone_id = aws_lb.alb.zone_id
+  }
+
+}
+
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = "api.${var.route53_zone_name}"
+  validation_method = "DNS"
+}
+
