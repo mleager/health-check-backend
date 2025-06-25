6838e847284342dae2a3d44ef9ef8a5f45ab40ea
mark <markleager92@gmail.com>
2025-06-24 23:36:18 -0400

    fix typo in route53_record resource

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,14 +2,15 @@ data "aws_route53_zone" "primary" {
   name = var.route53_zone_name
 }
 
-resource "aws_route53_recourd" "subdomain_record" {
+resource "aws_route53_record" "subdomain_record" {
   zone_id = data.aws_route53_zone.primary.id
-  name    = "api.${var.route53_zone}"
+  name    = "api.${var.route53_zone_name}"
   type    = "A"
 
   alias {
-    name    = aws_lb.alb.dns_name
-    zone_id = aws_lb.alb.zone_id
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
   }
 
 }
